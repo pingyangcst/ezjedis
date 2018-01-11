6608a4503a64ada76c88dfcba146ce1ad5dc5d7e
xjs1919 <xjs@163.com>
2018-01-11 14:08:29 +0800

    ignore+MainApplication

---
+++ src/test/java/com/github/xjs/ezjedis/MainApplication.java
@@ -0,0 +1,18 @@
+package com.github.xjs.ezjedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 605162215@qq.com
+ *
+ * @date 2018年1月11日 下午2:05:02<br/>
+ */
+@SpringBootApplication
+public class MainApplication {
+	
+	    public static void main(String[] args) throws Exception {
+	       	SpringApplication.run(MainApplication.class, args);
+	    }
+	    
+}
