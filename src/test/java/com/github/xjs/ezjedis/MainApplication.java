929893ac46a500e544b987a3ff5728fb41ab429a
xjs1919 <xjs@163.com>
2018-07-17 10:39:26 +0800

    test

--- src/test/java/com/github/xjs/ezjedis/MainApplication.java
+++ src/test/java/com/github/xjs/ezjedis/MainApplication.java
@@ -3,11 +3,14 @@ package com.github.xjs.ezjedis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.xjs.ezjedis.config.EnableEzJedis;
+
 /**
  * @author 605162215@qq.com
  *
  * @date 2018年1月11日 下午2:05:02<br/>
  */
+@EnableEzJedis
 @SpringBootApplication
 public class MainApplication {
 	    public static void main(String[] args) throws Exception {
