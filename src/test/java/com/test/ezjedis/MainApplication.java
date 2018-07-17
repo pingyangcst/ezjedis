c0410de5f2ec16cef523b4e2f93bd84c8e08cc83
xjs1919 <xjs@163.com>
2018-07-17 10:53:38 +0800

    EzJedisConfig

--- src/test/java/com/test/ezjedis/MainApplication.java
+++ src/test/java/com/test/ezjedis/MainApplication.java
@@ -9,6 +9,9 @@ import com.github.xjs.ezjedis.config.EnableEzJedis;
  * @author 605162215@qq.com
  *
  * @date 2018年1月11日 下午2:05:02<br/>
+ * 两种启用方式：
+ * 1.在入口类添加@EnableEzJedis注解
+ * 2.单独定义一个EzJedisConfig类，添加@EnableEzJedis注解
  */
 @EnableEzJedis
 @SpringBootApplication
---
+++ src/test/java/com/test/ezjedis/config/EzJedisConfig.java
@@ -0,0 +1,16 @@
+package com.test.ezjedis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.github.xjs.ezjedis.config.EnableEzJedis;
+
+/**
+ * @author 605162215@qq.com
+ *
+ * @date 2018年7月17日 上午10:51:19<br/>
+ */
+@EnableEzJedis
+@Configuration
+public class EzJedisConfig {
+	
+}
